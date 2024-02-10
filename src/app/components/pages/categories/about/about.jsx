0b58f37de6c10ef69a74a0a3351e64d28f9c7914
nonfermata <nonfermata@gmail.com>
2024-02-11 00:29:15 +0530

    10/02

--- src/app/components/pages/categories/about/about.jsx
+++ src/app/components/pages/categories/about/about.jsx
@@ -8,6 +8,7 @@ import rightImg from '../../../../assets/images/about-right.png';
 import SubMenu from './subMenu/subMenu';
 import { useParams } from 'react-router-dom';
 import SubCategory from './subCategories/subCategory';
+import audioGorod from '../../../../assets/audio/neobitaemy-gorod.mp3';
 
 const About = () => {
     const { subCutName } = useParams();
@@ -15,46 +16,52 @@ const About = () => {
     dispatch(setBrg(aboutBgr));
     const { vrezka, text } = about;
     return (
-        <div className={classes.aboutWrap}>
-            <div className={classes.aboutLeftContent}>
-                <div className={classes.catHeadWrap}>
-                    <div className='titleCategory'>О проекте</div>
-                    <SubMenu />
+        <>
+            <div className={classes.audioBox}>
+                <audio controls autoPlay src={audioGorod}></audio>
+            </div>
+            <div className={classes.aboutWrap}>
+                <div className={classes.aboutLeftContent}>
+                    <div className={classes.catHeadWrap}>
+                        <div className='titleCategory'>О проекте</div>
+                        <SubMenu />
+                    </div>
+                    {subCutName ? (
+                        <SubCategory subCatName={subCutName} />
+                    ) : (
+                        <>
+                            <p className={classes.vrezka}>{vrezka}</p>
+                            <div className={classes.text}>
+                                {text.map((item, index) => (
+                                    <p key={index}>{item}</p>
+                                ))}
+                            </div>
+                            <div className={classes.endLine}></div>
+                            <div className={classes.thanks}>
+                                Медиашкола Игоря Попова в Заполярье выражает
+                                искреннюю благодарность за многолетнюю поддержку
+                                и помощь департаменту общественных связей
+                                компании «Норникель», медиакомпании «Северный
+                                город» и лично Андрею Кирпичникову, Сергею
+                                Панченко, Татьяне Смирновой, Занде Бусловой,
+                                Вере Калабековой, Артуру Калабекову и Вере
+                                Скорницкой!
+                            </div>
+                            <div className={classes.dedication}>
+                                Эту книгу мы посвящаем памяти нашего большого
+                                друга и замечательного человека{' '}
+                                <span className='fw600'>
+                                    Сергея Николаевича Панченко.
+                                </span>
+                            </div>
+                        </>
+                    )}
                 </div>
-                {subCutName ? (
-                    <SubCategory subCatName={subCutName} />
-                ) : (
-                    <>
-                        <p className={classes.vrezka}>{vrezka}</p>
-                        <div className={classes.text}>
-                            {text.map((item, index) => (
-                                <p key={index}>{item}</p>
-                            ))}
-                        </div>
-                        <div className={classes.endLine}></div>
-                        <div className={classes.thanks}>
-                            Медиашкола Игоря Попова в Заполярье выражает
-                            искреннюю благодарность за многолетнюю поддержку и
-                            помощь департаменту общественных связей компании
-                            «Норникель», медиакомпании «Северный город» и лично
-                            Андрею Кирпичникову, Сергею Панченко, Татьяне
-                            Смирновой, Занде Бусловой, Вере Калабековой, Артуру
-                            Калабекову и Вере Скорницкой!
-                        </div>
-                        <div className={classes.dedication}>
-                            Эту книгу мы посвящаем памяти нашего большого друга
-                            и замечательного человека{' '}
-                            <span className='fw600'>
-                                Сергея Николаевича Панченко.
-                            </span>
-                        </div>
-                    </>
-                )}
+                <div className={classes.aboutRightContent}>
+                    <img src={rightImg} alt='image' />
+                </div>
             </div>
-            <div className={classes.aboutRightContent}>
-                <img src={rightImg} alt='image' />
-            </div>
-        </div>
+        </>
     );
 };
 
